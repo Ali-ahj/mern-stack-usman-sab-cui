03d4ffa1fa3689e8ada894d9a02f519d979f398c
Ali Hassan <bhailog9211.bhai@gmail.com>
2021-08-12 21:32:43 -0700

    Lecture6-EventBinding

---
+++ Lecture6-js6-eventBinding/myScripts.js
@@ -0,0 +1,3 @@
+function btnClicked(){
+    console.log("Button is Clicked");
+}
---
+++ Lecture6-js6-eventBinding/realExample.js
@@ -0,0 +1,9 @@
+function update(){
+    console.log("Updating results");
+    var input = document.getElementById('myinput');
+    console.log(input.value);
+
+    var output = document.getElementById('output');
+    output.innerHTML = input.value;
+    console.log(output.value);
+}
