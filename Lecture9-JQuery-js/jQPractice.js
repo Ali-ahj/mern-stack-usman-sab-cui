7d184f0f5d2f37c694f5b1f61fd1c2867da9424a
Ali Hassan <bhailog9211.bhai@gmail.com>
2021-08-14 07:43:43 -0700

    Lecture-9-JQuery(lec-1)

---
+++ Lecture9-JQuery-js/jQPractice.js
@@ -0,0 +1,27 @@
+$(function () {
+  //JQ will execute this function after page load
+  //So do your bindings
+  $("#addButton").click(handleBtnClick);
+  $("#todos li ").click(removeMe);
+  $();
+});
+
+function handleBtnClick() {
+  // alert("btn clicked");
+  var newTodo = $("#newTodo").val();
+  if (!newTodo) {
+    $("#newTodo").addClass("error");
+    return;
+  }
+  $("#newTodo").removeClass("error");
+
+  $("#newTodo").val(" ");
+  $("#todos").append("<li>" + newTodo + "</li>");
+  $("#todos li ").click(removeMe);
+  return;
+}
+
+function removeMe() {
+  $(this).fadeOut();
+  // .remove();
+}
