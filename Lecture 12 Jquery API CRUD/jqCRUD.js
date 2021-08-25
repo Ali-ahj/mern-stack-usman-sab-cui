c8480712945fdd4325cea45023bb90fbde8e4f8f
Ali Hassan <bhailog9211.bhai@gmail.com>
2021-08-25 04:36:42 -0700

    Lecture-12-CRUD(Method:GET)
    Lecture 12 JS 12 JQuery API Get Cal

--- Lecture 12 Jquery API CRUD/jqCRUD.js
+++ Lecture 12 Jquery API CRUD/jqCRUD.js
@@ -1,3 +1,26 @@
+
+// // $(function () {
+// //   loadRecipies();
+// // });
+// // function loadRecipies() {
+// //   $.ajax({
+// //     url: "https://usman-recipes.herokuapp.com/api/recipes",
+// //     method: "GET",
+// //     success: function (response) {
+// //       console.log(response);
+// //       var recipies = $("#recipies");
+// //       recipies.empty();
+// //       for (var i = 0; i < response.length; i++) {
+// //         var rec = response[i];
+// //         recipies.append(
+// //           `<div class ="recipie"><h3>"${rec.title}</h3><p><button class="btn btn-danger float-right">Delete</button>${rec.body}</p></div>`
+// //         );
+// //         // recipies.append("<div><h3>"+rec.title +"</h3></div>")
+// //       }
+// //     },
+// //   });
+// // }
+
 $(function(){
     loadRecipies();
 })
@@ -5,15 +28,62 @@ function loadRecipies(){
     $.ajax({
         url : "https://usman-recipes.herokuapp.com/api/recipes",
         method : "GET",
-        success : function (response){
+        success : function(response){
         console.log(response);
-        var recipies = $("#recipies");
+        var recipies = $("#recipies")
         recipies.empty();
-        for(var i=0 ; i<response.length ; i++){
+        for(var i =0 ; i<response.length;i++){
             var rec = response[i];
-            recipies.append(`<div class ="recipie"><h3>"${rec.title}</h3><p><button class="btn btn-danger float-right">Delete</button>${rec.body}</p></div>`);
-            // recipies.append("<div><h3>"+rec.title +"</h3></div>")
+            recipies.append(
+                `<div class = "recipie"><h3>${rec.title}</h3><p><button class = "btn btn-danger btn-sm float-right">Delete</button>${rec.body}</p></div>`);
+            // recipies.append("<div><h3>"+rec.title+"</h3></div>");
         }
+     
         }
-    })
+    });
 }
+
+// $(function(){
+//     loadRecipies();
+// })
+// function loadRecipies(){
+//     $.ajax({
+//         url : "https://usman-recipes.herokuapp.com/api/recipes",
+//         method : "GET",
+//         success:function (response){
+//         console.log(response);
+//         var recipies =  $("#recipies");
+//         recipies.empty()
+//         for(var i=0 ;i<response.length;i++){
+//             var rec = response[i];
+//             recipies.append(
+//                 `<div class = "recipie"><h3>${rec.title}</h3><p><button class = "btn btn-danger btn-sm float-right">Delete</button>${rec.body}</p></div>`);
+//         //  `<div class = "recipie"><h3>${rec.title}</h3><p><button class = "btn btn-danger btn-sm float-right">Delete</button>${rec.body}</p></div>`)
+//         }
+//         }
+       
+//     });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
